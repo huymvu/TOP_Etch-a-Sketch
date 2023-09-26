fc7e1a07fcfc8c7400ed4c0582d838fa860e2ce4
Huy <huyminhvu1996@gmail.com>
2023-09-25 20:26:04 -0700

    Implement basic sketch app
    Add event listener to the button. When user click the button, the
    browser will ask the user for the grid size then create the grid.
    Function changeSquareColor() will change the color of each square When
    mouse point to it.

---
+++ main.js
@@ -0,0 +1,28 @@
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+
+button.addEventListener('click', () => {
+    container.innerHTML = "";
+    let gridSize = prompt('Please enter the grid size (Maximum is 100)');
+    if (gridSize > 100) {
+        gridSize = prompt('That is too big. Please re-enter the grid size (Maximum is 100)');
+    }
+    let size = gridSize * gridSize;
+    for (let i = 0; i < size; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.width = `${320 / gridSize}px`;
+        grid.style.height = `${320 / gridSize}px`;
+        container.appendChild(grid);
+    };
+    changeSquareColor();
+})
+
+function changeSquareColor() {
+    const gridSquares = document.querySelectorAll(".grid");
+    gridSquares.forEach((square) => {
+        square.addEventListener('mouseenter', (e) => {
+            e.target.style.backgroundColor = "gray";
+        })
+    })
+}
